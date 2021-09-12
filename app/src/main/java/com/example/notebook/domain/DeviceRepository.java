aa3f7c06baaafd1d0fdf1cada13f6df00e018726
NikitaRybakov21 <nikitarybakov044@gmail.com>
2021-09-12 22:22:34 +0300

    dz_Notes

--- app/src/main/java/com/example/notebook/domain/DeviceRepository.java
+++ app/src/main/java/com/example/notebook/domain/DeviceRepository.java
@@ -10,9 +10,10 @@ public class DeviceRepository implements NoteRepository{
     public List<Note> getNote() {
         ArrayList<Note> notesList = new ArrayList<>();
 
-        notesList.add(new Note(R.string.note1,R.string.noteName1,R.string.noteDate1));
-        notesList.add(new Note(R.string.note2,R.string.noteName2,R.string.noteData2));
-        notesList.add(new Note(R.string.note3,R.string.noteName3,R.string.noteData3));
+        notesList.add(new Note(R.string.note1,R.string.noteName1,"https://look.com.ua/pic/201508/1280x1024/look.com.ua-127118.jpg"));
+        notesList.add(new Note(R.string.note2,R.string.noteName2,"https://lookw.ru/9/945/1566941301-oboi-008.jpg"));
+        notesList.add(new Note(R.string.note3,R.string.noteName3,"https://3mins.es/wp-content/uploads/2021/05/spacex-starship-scaled-1.jpg"));
+        notesList.add(new Note(R.string.note4,R.string.noteName4,"https://cdn.fishki.net/upload/post/2018/02/22/2519736/filin-sova-ptitsa-khishchnik-krylya-vzmakh-111969-1680x1050.jpg"));
 
         return notesList;
     }
